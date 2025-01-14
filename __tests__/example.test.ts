bfac0fda01c93ba17b2ecd64ba28e064f1290bf3
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2025-01-14 18:24:34 +0000

    Initial commit

---
+++ __tests__/example.test.ts
@@ -0,0 +1,5 @@
+import { add } from '../src/example';
+
+test('adds two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+});
---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
